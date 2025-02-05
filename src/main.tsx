5f8855d234eda24ab6ee661959b546dd1635f454
nmchugh17 <nmchu17@gmail.com>
2025-02-05 00:40:46 +0000

    Build responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { Provider } from "@/components/ui/provider"
+import { ChakraProvider } from '@chakra-ui/react'
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
+    </ChakraProvider>
   </React.StrictMode>,
 )
