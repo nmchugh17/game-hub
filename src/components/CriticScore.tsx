ed244652703b7535d163c4a600392943c1ed5555
nmchugh17 <nmchu17@gmail.com>
2025-02-09 14:51:10 +0000

    Get optimized images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,6 +5,7 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
+  // eslint-disable-next-line prefer-const
   let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
   return (
     <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
